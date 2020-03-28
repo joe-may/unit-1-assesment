ee88ea4b5828fcf0dfc1d116ddbaefc4dc3cc272
Joe May <joemaybooking@gmail.com>
2020-03-27 20:23:01 -0700

    functional but not pretty

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
+
+
 $( document ).ready(function() {
     console.log("ready!");
 
+
+
+/////////////////////////
 var topNumber = 0;
+var x = $('input').val();
+var xNum = parseInt(x);
+        console.log(xNum);
+
+////////////////////////
 
 
 function inputFunct() {
-    $('#top').append(topNumber);
+    $('.top').append("<p>" + topNumber + "</p>");
 }
 
 inputFunct();
 
 
-var x = $('input').val();
-var xNum = parseInt(x);
-        console.log(xNum);
 
+$('input').on('click',function() {
+    $('p').first().remove();
+});
 
 $('.plus').on('click',function() {
     var x = $('input').val();
@@ -22,7 +32,36 @@ $('.plus').on('click',function() {
     var x = $('input').val();
     topNumber += xNum;
     inputFunct();
+    blackNums();
 });
+
+$('.minus').on('click',function() {
+    var x = $('input').val();
+    var xNum = parseInt(x);
+    var x = $('input').val();
+    topNumber -= xNum;
+    inputFunct();
+    redNums();
+    blackNums();
+});
+
+function redNums() {
+    if(topNumber < 0) {
+        $('.top').css("color", "red");  
+    } else {
+       return; 
+    }
+}
+
+function blackNums() {
+    if(topNumber > 0) {
+        $('.top').css("color", "black");  
+    } else {
+       return; 
+    }
+}
+/////////////////////////
 
 
 });
+
